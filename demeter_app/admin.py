9de8ecce999670f9a9f25221ffcbe049bedb7cac
Carkzis <marc.jowett@gmail.com>
2021-03-12 00:20:42 +0000

    Added databases.

--- demeter_app/admin.py
+++ demeter_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Nation, Meal
+
+admin.site.register(Nation)
+admin.site.register(Meal)
--- demeter_app/models.py
+++ demeter_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Nation(models.Model):
+    """Contains a list of the countries."""
+    country = models.CharField(max_length=50, primary_key=True)
+    continent = models.CharField(max_length=20)
+    national_dish = models.CharField(max_length=50)
+
+class Meal(models.Model):
+    """Contains a list of meals and their details."""
+    member = models.ForeignKey(User, on_delete=models.CASCADE)
+    meal = models.CharField(max_length=50)
+    country = models.ForeignKey(Nation, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
