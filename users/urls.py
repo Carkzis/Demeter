4becd2c68b8e15bd0d9eea96d79a048503c34584
Carkzis <marc.jowett@gmail.com>
2021-03-14 20:59:28 +0000

    Fixed forgotten password issue.

--- users/urls.py
+++ users/urls.py
@@ -12,32 +12,4 @@ urlpatterns = [
     # Registration page.
     path('register/', views.register, name='register'),
     # Password reset urls.
-    path(
-        'password_reset',
-        auth_views.PasswordResetView.as_view(
-            template_name='registration/password_reset_form.html',
-            email_template_name='registration/password_reset_email.html',
-            success_url=reverse_lazy('users:password_reset_done')
-        ),
-        name='password_reset'
-    ),
-    path(
-        'password_reset/done/',
-        auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
-        name='password_reset_done'
-    ),
-    path(
-        'reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name='registration/password_reset_confirm.html',
-            success_url=reverse_lazy('users:password_reset_complete.html')
-        ),
-        name='password_reset_confirm'
-    ),
-    path(
-        'reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-        name='password_reset_complete'
-    ),
-
 ]
